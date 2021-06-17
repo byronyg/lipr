97f251ea4c83ee55f333cb36b9b1165946185f97
Byron Yego <byronyego@gmail.com>
2021-06-17 10:51:03 +0300

    updates

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       v-model="drawer"
       dark
       app
-      class="indigo darken-4"
+      color="#0C2556 "
     >
       <v-list>
 
@@ -35,6 +35,23 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      <v-subheader>REPORTS</v-subheader>
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          class="text--secondary"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -49,18 +66,12 @@
         <v-icon>mdi-cog-outline</v-icon>
       </v-btn>
 
-      <v-avatar
-        size="36"
-      >
-      <img
-        src="https://cdn.vuetifyjs.com/images/john.jpg"
-        alt="John"
-      >
-    </v-avatar>
 
       <v-menu
         left
         bottom
+        offset-y
+        :nudge-bottom="4"
       >
         <template v-slot:activator="{ on, attrs }">
           <v-btn
@@ -68,9 +79,36 @@
             v-bind="attrs"
             v-on="on"
           >
-            <v-icon>mdi-chevron-down</v-icon>
+      <v-avatar
+        size="42"
+      >
+      <img
+        src="https://i.imgur.com/73L3A4W.png"
+        alt="Your Company Name"
+      >
+    </v-avatar>
+            <!-- <v-icon>mdi-chevron-down</v-icon> -->
           </v-btn>
         </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-avatar>
+              <img
+                src="https://i.imgur.com/73L3A4W.png"
+                alt="Your Company Name"
+              >
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>Company Name</v-list-item-title>
+              <v-list-item-subtitle>Edit Profile</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
 
         <v-list>
           <v-list-item
@@ -100,7 +138,16 @@ export default {
     items: [
           { title: 'Dashboard', icon: 'mdi-view-dashboard' },
           { title: 'Analytics', icon: 'mdi-account-box' },
-          { title: 'Customers', icon: 'mdi-gavel' },
+          { title: 'Customers', icon: 'mdi-account-group-outline' },
+          { title: 'Payments', icon: 'mdi-credit-card-outline' },
+          { title: 'Reports', icon: 'mdi-file-edit-outline' },
+          { title: 'Marketing', icon: 'mdi-message-outline' },
+          { title: 'Edit Profile', icon: 'mdi-account-edit-outline' },
+          { title: 'Settings', icon: 'mdi-cog-outline' },
+          { title: 'Contact Us', icon: 'mdi-phone-outline' },
+          { title: 'Report a Problem', icon: 'mdi-alert' },
+          { title: 'FAQs', icon: 'mdi-frequently-asked-questions' },
+          { title: 'Terms & Conditions', icon: 'mdi-alert-circle-outline' },
         ]
   }),
 };
